c07538d821dcc341daf9200f50d3c062cb20b8a9
fotios <fotios@fundalytica.com>
2020-11-11 05:52:04 +0000

    refactoring and adding USTradingCalendar

--- stock.py
+++ stock.py
@@ -1,12 +1,23 @@
 import re
-
 def valid_symbol(symbol):
     STOCK_SYMBOL_PATTERN = "[A-Z]{1,4}((\.){1}[A|B]{1}){0,1}$"
-
     pattern = re.compile(STOCK_SYMBOL_PATTERN)
     match = pattern.match(symbol)
+    return match is not None
 
-    if match is None:
-        return False
-
-    return True
+import pandas as pd
+from pandas.tseries.holiday import AbstractHolidayCalendar, Holiday, nearest_workday, USMartinLutherKingJr, USPresidentsDay, USMemorialDay, USLaborDay, USThanksgivingDay, GoodFriday
+def USTradingCalendar():
+    class USTradingCalendar(AbstractHolidayCalendar):
+        rules = [
+            Holiday('NewYearsDay', month=1, day=1, observance=nearest_workday),
+            Holiday('USIndependenceDay', month=7, day=4, observance=nearest_workday),
+            Holiday('Christmas', month=12, day=25, observance=nearest_workday),
+            USMartinLutherKingJr,
+            USPresidentsDay,
+            GoodFriday,
+            USMemorialDay,
+            USLaborDay,
+            USThanksgivingDay
+        ]
+    return USTradingCalendar()
