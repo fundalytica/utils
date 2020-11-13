477246537720ed46956da200833956d606299a6a
fotios <fotios@fundalytica.com>
2020-11-13 10:54:02 +0000

    removed valid_arg_count methid, added confirmation prompt method

--- utils.py
+++ utils.py
@@ -1,3 +1,20 @@
-# import sys
-# def valid_arg_count(count = 2):
-#     return (len(sys.argv) == count)
+from distutils import util
+def confirm(question, default='no'):
+    if default is None:
+        prompt = " [y/n] "
+    elif default == 'yes':
+        prompt = " [Y/n] "
+    elif default == 'no':
+        prompt = " [y/N] "
+    else:
+        raise ValueError(f"Unknown setting '{default}' for default.")
+
+    while True:
+        try:
+            resp = input(question + prompt).strip().lower()
+            if default is not None and resp == '':
+                return default == 'yes'
+            else:
+                return util.strtobool(resp)
+        except ValueError:
+            print("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
