c420a06ea98fe338971028ea320a72a14f67a69d
fotios <fotios@fundalytica.com>
2020-11-20 07:35:40 +0000

    moved imports to top

--- stock.py
+++ stock.py
@@ -1,12 +1,14 @@
 import re
+
+import pandas as pd
+from pandas.tseries.holiday import AbstractHolidayCalendar, Holiday, nearest_workday, USMartinLutherKingJr, USPresidentsDay, USMemorialDay, USLaborDay, USThanksgivingDay, GoodFriday
+
 def valid_symbol(symbol):
     STOCK_SYMBOL_PATTERN = "[A-Z]{1,4}((\.){1}[A|B]{1}){0,1}$"
     pattern = re.compile(STOCK_SYMBOL_PATTERN)
     match = pattern.match(symbol)
     return match is not None
 
-import pandas as pd
-from pandas.tseries.holiday import AbstractHolidayCalendar, Holiday, nearest_workday, USMartinLutherKingJr, USPresidentsDay, USMemorialDay, USLaborDay, USThanksgivingDay, GoodFriday
 def USTradingCalendar():
     class USTradingCalendar(AbstractHolidayCalendar):
         rules = [
@@ -21,6 +23,7 @@ def USTradingCalendar():
             USThanksgivingDay
         ]
     return USTradingCalendar()
+
 def USOtherNonTradingDates():
     # December 5, 2018: National Day of Mourning for George H.W. Bush
     return ['2018-12-05']
