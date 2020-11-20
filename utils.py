c652666b42d93ea859aac9064910f1e5ac54b021
fotios <fotios@fundalytica.com>
2020-11-20 07:35:55 +0000

    added file path, name and extension methods

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
+import os
 import sys
 import json
-
+import pathlib
 from distutils import util
+
 from colorama import Style
 
 # user confirmation prompt
@@ -25,18 +27,27 @@ def confirm(question, default='no'):
         except ValueError:
             print("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
 
+# running from terminal
+def terminal():
+    return sys.stdin.isatty()
+
+def file_path(file):
+    return pathlib.Path(file).parent.absolute()
+
+def file_name(file):
+    return os.path.splitext(file)[0]
+
+def file_extension(file):
+    return os.path.splitext(file)[1]
+
 # color print
 def cprint(text, color):
     print(f'{color}{text}{Style.RESET_ALL}')
 
-# using tty
-def terminal():
-    return sys.stdin.isatty()
+# pretty print
+def pprint(obj):
+    print(json.dumps(obj, indent=4, sort_keys=True))
 
 # data size
 def size(data):
     return f'{(sys.getsizeof(data) / 1024 / 1024):.2f} MB'
-
-# pretty print
-def pprint(obj):
-    print(json.dumps(obj, indent=4, sort_keys=True))
