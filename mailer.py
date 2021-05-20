6abc78ffbe00db308ad05e0c60e019792f77d0e6
fotios <fotios@fundalytica.com>
2021-05-20 02:49:59 +0000

    mailgun mailer

---
+++ mailer.py
@@ -0,0 +1,43 @@
+# Documentation
+# https://documentation.mailgun.com/en/latest/user_manual.html#sending-via-api
+
+import yaml
+import requests
+
+import utils
+
+MAILGUN_URL = "https://api.mailgun.net/v3" # EU "https://api.eu.mailgun.net/v3"
+
+def get_config(parameter):
+    with open('/secret/mailgun.yml', 'r') as file:
+        config = yaml.load(file, yaml.Loader)
+        return config[parameter]
+
+def send_simple_message(sender, recipients, subject, text, sender_name=None, admin=None):
+    # add name to sender
+    if sender_name:
+        sender = f'{sender_name} <{sender}>'
+
+    # add admin to recipients
+    if admin:
+        recipients.append(admin)
+
+    recipients = utils.unique(recipients)
+
+    url = f'{MAILGUN_URL}/{get_config("domain")}/messages'
+    auth = ("api", get_config('api_key'))
+    data = {"from": sender, "to": recipients, "subject": subject, "text": text}
+
+    return requests.post(url, auth=auth, data=data)
+
+sender = get_config('default_sender_email')
+recipients = [get_config('admin')]
+# recipients = ['test-zt9loea83@srv1.mail-tester.com']
+subject = 'Hello'
+text = 'This is Mailgun!'
+response = send_simple_message(sender, recipients, subject, text)
+# response = send_simple_message(sender, recipients, subject, text, admin=get_config('admin'))
+# response = send_simple_message(sender, recipients, subject, text, sender_name=get_config('default_sender_name'))
+
+print(response)
+print(response.text)
